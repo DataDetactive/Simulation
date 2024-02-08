35cd45bd5f7820b600d4170ad2199b146fa38f35
DataDetactive <kareemelgohary98@gmail.com>
2024-02-08 10:00:06 +0100

    check

--- src/animationloop/DefaultAnimationLoop.cpp
+++ src/animationloop/DefaultAnimationLoop.cpp
@@ -7,7 +7,14 @@
 
 
 //// TODO : implement this function
-//void DefaultAnimationLoop::step(double time) {}
+void DefaultAnimationLoop::step(double time) {
+    // create a member of the class that we have just created in the animation.h file
+    ApplyIntegratorVisitor a(this->getContext()->getDt());
+    // carry out the process
+    this->getContext()->processVisitor(a);
+     ApplyMappingVisitor b;
+     this->getContext()->processVisitor(b);
+}
 
 DECLARE_CLASS(DefaultAnimationLoop)
 
--- src/animationloop/DefaultAnimationLoop.h
+++ src/animationloop/DefaultAnimationLoop.h
@@ -2,11 +2,51 @@
 #define DEFAULTANIMATIONLOOP_H
 
 #include <animationloop/AnimationLoop.h>
+#include <core/Visitor.h>
+#include<integrator/Integrator.h>
+#include<mapping/Mapping.h>
+
+#include <animationloop/AnimationLoop.h>
 
 class DefaultAnimationLoop : public AnimationLoop {
 public:
 
     void step(double time);
+
+};
+// create visitors allowing the function to be performed each time it is needed.
+class ApplyIntegratorVisitor : public Visitor{
+    public:
+    ApplyIntegratorVisitor(double t) : Visitor(true) {
+            m_t = t;
+        }
+    // check if it is an integrator
+    bool processObject(BaseObject * o) {
+            if (dynamic_cast<Integrator *>(o)) {
+                ((Integrator *) o)->step(m_t);
+            }
+
+            return true;
+        }
+    double m_t;
+};
+
+
+class ApplyMappingVisitor : public Visitor {
+public:
+
+    ApplyMappingVisitor() : Visitor(true) {
+
+    }
+
+    bool processObject(BaseObject * o) {
+        if (dynamic_cast<Mapping *>(o)) {
+            ((Mapping *) o)->apply();
+        }
+
+        return true;
+    }
+
 
 };
 #endif
